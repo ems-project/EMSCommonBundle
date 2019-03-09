ee732a58b80aa07582318a7de140270859aa4f25
theus deka <me@theus.be>
2019-03-09 21:41:57 +0100

    EmsConst class for ems field name

---
+++ Helper/EmsConst.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theus
+ * Date: 09/03/2019
+ * Time: 14:04
+ */
+
+namespace EMS\CommonBundle\Helper;
+
+
+class EmsConst
+{
+    const CONTENT_MIME_TYPE_FIELD = 'mimetype';
+    const CONTENT_FILE_HASH_FIELD = 'sha1';
+    const CONTENT_FILE_SIZE_FIELD = 'filesize';
+    const CONTENT_FILE_NAME_FIELD = 'filename';
+
+}
