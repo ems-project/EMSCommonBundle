a59e4b495da71f7cf0be5a0d4a133618299d99ca
theus deka <me@theus.be>
2019-03-11 23:47:08 +0100

    All asset config constants have been defined

--- Helper/EmsFields.php
+++ Helper/EmsFields.php
@@ -9,5 +9,21 @@ final class EmsFields
     const CONTENT_FILE_HASH_FIELD = 'sha1';
     const CONTENT_FILE_SIZE_FIELD = 'filesize';
     const CONTENT_FILE_NAME_FIELD = 'filename';
+    const CONTENT_PUBLISHED_DATETIME_FIELD = '_published_datetime';
+
+    const ASSET_CONFIG_DISPOSITION = '_disposition';
+    const ASSET_CONFIG_BACKGOUND = '_background';
+    const ASSET_CONFIG_TYPE = '_config_type';
+    const ASSET_CONFIG_TYPE_IMAGE = 'image';
+    const ASSET_CONFIG_GRAVITY = '_gravity';
+    const ASSET_CONFIG_MIME_TYPE = '_mime_type';
+    const ASSET_CONFIG_HEIGHT = '_height';
+    const ASSET_CONFIG_QUALITY = '_quality';
+    const ASSET_CONFIG_RESIZE = '_resize';
+    const ASSET_CONFIG_WIDTH = '_width';
+    const ASSET_CONFIG_RADIUS = '_radius';
+    const ASSET_CONFIG_RADIUS_GEOMETRY = '_radius_geometry';
+    const ASSET_CONFIG_BORDER_COLOR = '_border_color';
+    const ASSET_CONFIG_WATERMARK_HASH = '_watermark_hash';
 
 }
