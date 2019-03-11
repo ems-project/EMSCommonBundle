15c05acd6f5771d6f4d12493e474caaa3d09254e
theus deka <me@theus.be>
2019-03-11 21:08:27 +0100

    normalizeArray is always returning true: useless

--- Helper/ArrayTool.php
+++ Helper/ArrayTool.php
@@ -3,38 +3,8 @@
 namespace EMS\CommonBundle\Helper;
 
 
-
 class ArrayTool
 {
-
-    /**
-     * Normalize an array in order to compute it's hash
-     *
-     * @param array $array
-     * @param int $sort_flags
-     * @return bool
-     */
-    public static function normalizeArray(array &$array, int $sort_flags = SORT_REGULAR)
-    {
-        if (!is_array($array)) {
-            trigger_error("Incorrect parameters, arrays expected", E_USER_ERROR);
-            return false;
-        }
-
-        $out = true;
-        ksort($array, $sort_flags);
-
-        foreach ($array as $index => &$arr) {
-            if( is_array($arr) && ! ArrayTool::normalizeArray($arr, $sort_flags)){
-                $out = false;
-            }
-
-            if(is_array($array[$index]) && empty($array[$index])) {
-                unset($array[$index]);
-            }
-        }
-        return $out;
-    }
 
     /**
      * Normalize and json encode an array in order to compute it's hash
@@ -43,10 +13,31 @@ class ArrayTool
      * @param int $jsonEncodeOptions
      * @return false|string
      */
-    public static function normalizeAndSerializeArray (array &$array, int $sort_flags = SORT_REGULAR, int $jsonEncodeOptions = 0)
+    public static function normalizeAndSerializeArray(array &$array, int $sort_flags = SORT_REGULAR, int $jsonEncodeOptions = 0)
     {
         ArrayTool::normalizeArray($array, $sort_flags);
         return json_encode($array, $jsonEncodeOptions);
+    }
+
+    /**
+     * Normalize an array in order to compute it's hash
+     *
+     * @param array $array
+     * @param int $sort_flags
+     */
+    public static function normalizeArray(array &$array, int $sort_flags = SORT_REGULAR)
+    {
+        ksort($array, $sort_flags);
+
+        foreach ($array as $index => &$arr) {
+            if (is_array($arr)) {
+                ArrayTool::normalizeArray($arr, $sort_flags);
+            }
+
+            if (is_array($array[$index]) && empty($array[$index])) {
+                unset($array[$index]);
+            }
+        }
     }
 
 
