f22cc48e1e84d74f898da1766c2f915ad1b41e77
theus deka <me@theus.be>
2019-05-10 07:59:28 +0200

    Remove comments and setters

--- Entity/Document.php
+++ Entity/Document.php
@@ -20,59 +20,19 @@ class Document
         $this->source = $source;
     }
 
-    /**
-     * @return string
-     */
     public function getContentType(): string
     {
         return $this->contentType;
     }
 
-    /**
-     * @param string $contentType
-     * @return Document
-     */
-    public function setContentType(string $contentType): Document
-    {
-        $this->contentType = $contentType;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
     public function getOuuid(): string
     {
         return $this->ouuid;
     }
 
-    /**
-     * @param string $ouuid
-     * @return Document
-     */
-    public function setOuuid(string $ouuid): Document
-    {
-        $this->ouuid = $ouuid;
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
     public function getSource(): array
     {
         return $this->source;
     }
-
-    /**
-     * @param array $source
-     * @return Document
-     */
-    public function setSource(array $source): Document
-    {
-        $this->source = $source;
-        return $this;
-    }
-
 
 }
