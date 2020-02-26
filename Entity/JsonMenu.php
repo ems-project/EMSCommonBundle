e67bf4cfc2893fdbec151fe4f40683cbca78fb2e
theus77 <me@theus.be>
2020-02-26 10:25:00 +0100

    Ems json menu decode (#58)
    * New JsonMenu class decoder
    * New Decoder service
    * New ems_json_menu_decode twig filter
    * David's and Jerome's remarks

---
+++ Entity/JsonMenu.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace EMS\CommonBundle\Entity;
+
+class JsonMenu
+{
+    /** @var string */
+    private $json;
+    /** @var string */
+    private $glue;
+    /** @var array */
+    private $structure;
+    /** @var array */
+    private $slugs;
+    /** @var array */
+    private $bySlugs;
+
+
+    public function __construct(string $source, string $glue)
+    {
+        $this->json = $source;
+        $this->glue = $glue;
+        $this->structure = json_decode($source, true);
+        $this->slugs = [];
+        $this->bySlugs = [];
+        $this->recursiveWalk($this->structure);
+    }
+
+    private function recursiveWalk(array $menu, string $basePath = '')
+    {
+        foreach ($menu as $item) {
+            $slug = $basePath . $item['label'];
+            $this->slugs[$item['id']] = $slug;
+            $this->bySlugs[$slug] = $item;
+            if (isset($item['children'])) {
+                $this->recursiveWalk($item['children'], $slug . $this->glue);
+            }
+        }
+    }
+
+    public function getBySlug(string $slug): array
+    {
+        return $this->bySlugs[$slug] ?? [];
+    }
+
+    public function getSlug(string $id): ?string
+    {
+        return $this->slugs[$id] ?? null;
+    }
+
+    public function getUids(): array
+    {
+        return array_keys($this->slugs);
+    }
+
+    public function getSlugs(): array
+    {
+        return array_values($this->slugs);
+    }
+
+    public function getJson(): string
+    {
+        return $this->json;
+    }
+
+    public function getStructure(): array
+    {
+        return $this->structure;
+    }
+
+    public function getGlue(): string
+    {
+        return $this->glue;
+    }
+}
