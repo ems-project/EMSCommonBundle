b90c60912a503fb2ef4a34014d10e8286705ee6f
David Meert <meertdavid@gmail.com>
2020-03-31 10:57:06 +0200

    bug(property-type): DateTime can return false, throws error now

---
+++ Exception/DateTimeCreationException.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CommonBundle\Exception;
+
+use Throwable;
+
+final class DateTimeCreationException extends \RuntimeException
+{
+    private function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public static function fromArray(array $data, string $key, $code = 0, Throwable $previous = null)
+    {
+        $input = $data[$key] ?? '[ERROR: key out of bound]';
+        $message = sprintf('Could not create a DateTime from input value: "%s", with key: "%s"', $input, $key);
+
+        return new static($message, $code, $previous);
+    }
+}
