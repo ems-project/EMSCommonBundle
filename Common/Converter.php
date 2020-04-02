bcafb7da6cc095961b786b2d25c0037995106c38
David Meert <meertdavid@gmail.com>
2020-04-02 08:35:57 +0200

    bug(param-type): preg_replace can return null

--- Common/Converter.php
+++ Common/Converter.php
@@ -26,9 +26,9 @@ class Converter
             $clean = str_replace($a, $b, $str);
         }
 
-        $clean = preg_replace("/[^a-zA-Z0-9\_\|\ \-\.]/", '', $clean);
+        $clean = (string) preg_replace("/[^a-zA-Z0-9\_\|\ \-\.]/", '', $clean);
         $clean = strtolower(trim($clean, '-'));
-        $clean = preg_replace("/[\/\_\|\ \-]+/", '-', $clean);
+        $clean = (string) preg_replace("/[\/\_\|\ \-]+/", '-', $clean);
 
         return $clean;
     }
