9f8dc2f0c47688da39d02308d48199c3e2246759
theus deka <me@theus.be>
2020-05-28 07:40:11 +0200

    HttpStorage fixes

--- Storage/Service/HttpStorage.php
+++ Storage/Service/HttpStorage.php
@@ -6,30 +6,19 @@ use EMS\CommonBundle\Common\HttpClientFactory;
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\StreamInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class HttpStorage extends AbstractUrlStorage
 {
 
-    /**
-     * @var string
-     */
+    /** @var string */
     private $baseUrl;
-    /**
-     * @var string
-     */
+    /** @var string */
     private $getUrl;
-    /**
-     * @var null|string
-     */
+    /** @var null|string */
     private $authKey;
 
-
-    /**
-     * HttpStorage constructor.
-     * @param string $baseUrl
-     * @param string $getUrl
-     * @param null|string $authKey
-     */
     public function __construct(string $baseUrl, string $getUrl, ?string $authKey = null)
     {
         $this->baseUrl = $baseUrl;
@@ -39,42 +28,23 @@ class HttpStorage extends AbstractUrlStorage
 
     private function getClient(): Client
     {
-        static $client = false;
-
-        if (!$client) {
+        static $client = null;
+        if ($client === null) {
             $client = HttpClientFactory::create($this->baseUrl, [], 30, true);
         }
-
         return $client;
     }
 
-
-
-    /**
-     * @inheritdoc
-     */
     protected function getBaseUrl(): string
     {
         return $this->baseUrl . $this->getUrl;
     }
 
-
-    /**
-     * @inheritdoc
-     */
-    protected function getPath(string $hash, ?string $cacheContext = null, bool $confirmed = true, string $ds = '/'): string
+    protected function getPath(string $hash, bool $confirmed = true, string $ds = '/'): string
     {
-        if ($cacheContext) {
-            return $this->baseUrl . '/asset/' . urlencode($cacheContext) . '/' . $hash;
-        }
         return $this->baseUrl . $this->getUrl . $hash;
     }
 
-
-
-    /**
-     * @inheritdoc
-     */
     public function health(): bool
     {
         try {
@@ -90,35 +60,16 @@ class HttpStorage extends AbstractUrlStorage
         return false;
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function read(string $hash, ?string $cacheContext = null, bool $confirmed = true)
+    public function read(string $hash, bool $confirmed = true): StreamInterface
     {
-        if ($cacheContext) {
-            return false;
-        }
-
         try {
             return $this->getClient()->get($this->getUrl . $hash)->getBody();
-        } catch (Exception $e) {
-            return false;
+        } catch (\Exception $e) {
+            throw new NotFoundHttpException($hash);
         }
     }
 
-
-    /**
-     * @inheritdoc
-     */
-    public function supportCacheStore(): bool
-    {
-        return false;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function initUpload(string $hash, int $size, string $name, string $type, ?string $context = null): bool
+    public function initUpload(string $hash, int $size, string $name, string $type): bool
     {
         try {
             $result = $this->getClient()->post('/api/file/init-upload/' . urlencode($hash) . '/' . $size . '?name=' . urlencode($name) . '&type=' . urlencode($type), [
@@ -130,13 +81,10 @@ class HttpStorage extends AbstractUrlStorage
 
             return $result->getStatusCode() === 200;
         } catch (\Exception $e) {
-            return false;
+            throw new NotFoundHttpException($hash);
         }
     }
 
-    /**
-     * @inheritdoc
-     */
     public function addChunk(string $hash, string $chunk, ?string $context = null): bool
     {
         try {
@@ -148,46 +96,26 @@ class HttpStorage extends AbstractUrlStorage
             ]);
             return $result->getStatusCode() === 200;
         } catch (\Exception $e) {
-            return false;
+            throw new NotFoundHttpException($hash);
         }
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function finalizeUpload(string $hash, ?string $context = null): bool
+    public function finalizeUpload(string $hash): bool
     {
-        return $this->head($hash, $context);
+        return $this->head($hash);
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function head(string $hash, ?string $cacheContext = null): bool
+    public function head(string $hash): bool
     {
-        if ($cacheContext) {
-            return false;
-        }
-
         try {
             return $this->getClient()->head($this->getUrl . $hash)->getStatusCode() === 200;
         } catch (Exception $e) {
-            return false;
+            throw new NotFoundHttpException($hash);
         }
     }
 
-    /**
-     * @param string $hash
-     * @param string $filename
-     * @param null|string $cacheContext
-     * @return bool
-     */
-    public function create(string $hash, string $filename, ?string $cacheContext = null): bool
+    public function create(string $hash, string $filename): bool
     {
-        if ($cacheContext) {
-            return false;
-        }
-
         try {
             $this->getClient()->request('POST', '/api/file', [
                 'multipart' => [
@@ -208,96 +136,33 @@ class HttpStorage extends AbstractUrlStorage
         }
     }
 
-    /**
-     * @param string $hash
-     * @param null|string $cacheContext
-     * @return null|int
-     */
-    public function getSize(string $hash, ?string $cacheContext = null): ?int
+    public function getSize(string $hash): int
     {
-        if ($cacheContext) {
-            return null;
-        }
-
-        //https://stackoverflow.com/questions/1545432/what-is-the-easiest-way-to-use-the-head-command-of-http-in-php?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
         try {
             $context = stream_context_create(array('http' => array('method' => 'HEAD')));
             $fd = fopen($this->baseUrl . $this->getUrl . $hash, 'rb', false, $context);
 
             if ($fd === false) {
-                return null;
+                throw new NotFoundHttpException($hash);
             }
             
             $metas = stream_get_meta_data($fd);
-            if (isset($metas['wrapper_data'])) {
-                foreach ($metas['wrapper_data'] as $meta) {
-                    if (preg_match('/^content\-length: (.*)$/i', $meta, $matches, PREG_OFFSET_CAPTURE)) {
-                        return intval($matches[1][0]);
-                    }
+            foreach ($metas['wrapper_data'] ?? [] as $meta) {
+                if (preg_match('/^content\-length: (.*)$/i', $meta, $matches, PREG_OFFSET_CAPTURE)) {
+                    return intval($matches[1][0]);
                 }
             }
         } catch (Exception $e) {
-            return null;
         }
+        throw new NotFoundHttpException($hash);
     }
 
-
-    /**
-     * @deprecated
-     * @param string $hash
-     * @param null|string $context
-     * @return \DateTime|null
-     */
-    public function getLastUpdateDate(string $hash, ?string $context = null): ?\DateTime
-    {
-        @trigger_error("getLastUpdateDate is deprecated.", E_USER_DEPRECATED);
-        if ($context) {
-            return null;
-        }
-
-        //https://stackoverflow.com/questions/1545432/what-is-the-easiest-way-to-use-the-head-command-of-http-in-php?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-        try {
-            $context = stream_context_create(array('http' => array('method' => 'HEAD')));
-            $fd = fopen($this->baseUrl . $this->getUrl . $hash, 'rb', false, $context);
-
-            if ($fd === false) {
-                return null;
-            }
-
-            $metas = stream_get_meta_data($fd);
-            if (isset($metas['wrapper_data'])) {
-                foreach ($metas['wrapper_data'] as $meta) {
-                    if (preg_match('/^Last\-Modified: (.*)$/', $meta, $matches, PREG_OFFSET_CAPTURE)) {
-                        $time = strtotime($matches[1][0]);
-                        if ($time === false) {
-                            $now = \DateTime::createFromFormat('U', (string) strtotime('now'));
-                            return $now === false ? null : $now;
-                        }
-                        $dateTime = \DateTime::createFromFormat('U', (string) $time);
-                        return $dateTime === false ? null : $dateTime;
-                    }
-                }
-            }
-        } catch (Exception $e) {
-            //So it's a NULL
-        }
-        return null;
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function __toString(): string
     {
         return HttpStorage::class . " ($this->baseUrl)";
     }
 
-
-    /**
-     * @param string $hash
-     * @return bool
-     */
-    public function remove(string $hash, ?string $context = null): bool
+    public function remove(string $hash): bool
     {
         return false;
     }
