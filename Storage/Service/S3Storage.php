be2c8eaf6b94a7c6c54e2ad14f730281aefbb151
theus deka <me@theus.be>
2020-05-28 07:40:35 +0200

    S3Storage fixes

--- Storage/Service/S3Storage.php
+++ Storage/Service/S3Storage.php
@@ -8,19 +8,13 @@ use AwsServiceBuilder;
 class S3Storage extends AbstractUrlStorage
 {
 
-    /**
-     * @var S3Client
-     */
+    /** @var S3Client*/
     private $s3Client = null;
 
-    /**
-     * @var string
-     */
+    /** @var string */
     private $bucket;
 
-    /**
-     * @var array
-     */
+    /** @var array */
     private $credentials;
 
     public function __construct(array $s3Credentials, string $s3Bucket)
@@ -29,10 +23,6 @@ class S3Storage extends AbstractUrlStorage
         $this->credentials = $s3Credentials;
     }
 
-
-    /**
-     * @inheritdoc
-     */
     protected function getBaseUrl(): string
     {
         if ($this->s3Client === null) {
@@ -42,27 +32,15 @@ class S3Storage extends AbstractUrlStorage
         return "s3://$this->bucket";
     }
 
-
-    /**
-     * @inheritdoc
-     */
     public function __toString(): string
     {
         return S3Storage::class . " ($this->bucket)";
     }
 
 
-    /**
-     * @param string $hash
-     * @param int $size
-     * @param string $name
-     * @param string $type
-     * @param null|string $context
-     * @return bool
-     */
-    public function initUpload(string $hash, int $size, string $name, string $type, ?string $context = null): bool
+    public function initUpload(string $hash, int $size, string $name, string $type): bool
     {
-        $path = $this->getPath($hash, $context, false);
+        $path = $this->getPath($hash, false);
         $result = $this->s3Client->putObject([
             'Bucket'     => $this->bucket,
             'Key'        => substr($path, 1 + strlen($this->getBaseUrl())),
@@ -73,18 +51,10 @@ class S3Storage extends AbstractUrlStorage
         return $result->hasKey('ETag');
     }
 
-
-
-    /**
-     * @param string      $hash
-     * @param string|null $context
-     *
-     * @return bool
-     */
-    public function finalizeUpload(string $hash, ?string $context = null): bool
+    public function finalizeUpload(string $hash): bool
     {
-        $out = parent::finalizeUpload($hash, $context);
-        $path = $this->getPath($hash, $context, false);
+        $out = parent::finalizeUpload($hash);
+        $path = $this->getPath($hash, false);
         $this->s3Client->deleteObject([
             'Bucket'     => $this->bucket,
             'Key'        => substr($path, 1 + strlen($this->getBaseUrl()))
