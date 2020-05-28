31d45f0ff22859d4dbed9edc1b8d920b805bb877
theus deka <me@theus.be>
2020-05-28 07:39:18 +0200

    FileSystemStorage cleaned

--- Storage/Service/FileSystemStorage.php
+++ Storage/Service/FileSystemStorage.php
@@ -5,40 +5,23 @@ namespace EMS\CommonBundle\Storage\Service;
 class FileSystemStorage extends AbstractUrlStorage
 {
 
-    /**
-     * Store the base url or the directory path
-     * @var string
-     */
+    /** @var string */
     private $storagePath;
 
-    /**
-     * Just an exception for the windows systems
-     * @var string
-     */
+    /** @var string */
     private $directorySeparator;
 
-    /**
-     * FileSystemStorage constructor.
-     * @param string $storagePath
-     * @param string $directorySeparator
-     */
     public function __construct(string $storagePath, string $directorySeparator = DIRECTORY_SEPARATOR)
     {
         $this->storagePath = $storagePath;
         $this->directorySeparator = $directorySeparator;
     }
 
-    /**
-     * @inheritdoc
-     */
     protected function getBaseUrl(): string
     {
         return $this->storagePath;
     }
 
-    /**
-     * @inheritdoc
-     */
     public function __toString(): string
     {
         return FileSystemStorage::class . " ($this->storagePath)";
