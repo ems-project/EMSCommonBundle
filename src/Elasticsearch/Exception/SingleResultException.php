1bddc74bdcea68c2dca0b31d4184638df17b9752
Mathieu De Keyzer <me@theus.be>
2020-11-22 08:17:02 +0100

    ref: Better SingleResultException

--- src/Elasticsearch/Exception/SingleResultException.php
+++ src/Elasticsearch/Exception/SingleResultException.php
@@ -1,11 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EMS\CommonBundle\Elasticsearch\Exception;
 
 class SingleResultException extends \Exception
 {
-    public function __construct(int $count)
+    /** @var int */
+    private $total;
+
+    public function __construct(int $total)
     {
-        parent::__construct(\sprintf('Single result exception: 1 result was expected, got %d', $count));
+        $this->total = $total;
+        parent::__construct(\sprintf('Single result exception: 1 result was expected, got %d', $total));
+    }
+
+    public function getTotal(): int
+    {
+        return $this->getTotal();
     }
 }
