ff69b9eb37b026c27937329b8a4d334110f54119
Laurens Mertens <laurensmertens@yahoo.com>
2021-03-16 09:40:14 +0100

    fix: use reflection class for private function

--- tests/Unit/Common/SpreadsheetGeneratorTest.php
+++ tests/Unit/Common/SpreadsheetGeneratorTest.php
@@ -19,7 +19,29 @@ class SpreadsheetGeneratorTest extends TestCase
     public function testConfigToSpreadsheet(): void
     {
         $config = \json_decode('{"filename":"export","writer":"xlsx","active_sheet":0,"sheets":[{"name":"Export form","color":"#FF0000","rows":[["apple","banana"],["pineapple","strawberry"]]},{"name":"Export form sheet 2","rows":[["a1","a2"],["b1","b3"]]}]}', true);
-        $this->assertSame('Export form', $this->spreadSheetGenerator->buildUpSheets($config)->getActiveSheet()->getTitle());
-        $this->assertSame('pineapple', $this->spreadSheetGenerator->buildUpSheets($config)->getActiveSheet()->getCell('A2')->getValue());
+        $this->assertSame('Export form', $this->callMethod($this->spreadSheetGenerator, 'buildUpSheets', [$config])->getActiveSheet()->getTitle());
+        $this->assertSame('pineapple', $this->callMethod($this->spreadSheetGenerator, 'buildUpSheets', [$config])->getActiveSheet()->getCell('A2')->getValue());
+    }
+
+    /**
+     * @param $object
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     * @throws \Exception
+     */
+    private function callMethod($object, string $method , array $parameters = [])
+    {
+        try {
+            $className = get_class($object);
+            $reflection = new \ReflectionClass($className);
+        } catch (\ReflectionException $e) {
+            throw new \Exception($e->getMessage());
+        }
+
+        $method = $reflection->getMethod($method);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
     }
 }
