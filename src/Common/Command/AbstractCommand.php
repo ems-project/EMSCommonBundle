b4a653301ec18a6cb2558101c19ea1056c1b0b6a
David mattei <d.mattei89@gmail.com>
2021-05-27 11:41:59 +0200

    feat: run and execute command

--- src/Common/Command/AbstractCommand.php
+++ src/Common/Command/AbstractCommand.php
@@ -6,19 +6,31 @@ namespace EMS\CommonBundle\Common\Command;
 
 use EMS\CommonBundle\Command\CommandInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProcessHelper;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
 
 abstract class AbstractCommand extends Command implements CommandInterface
 {
     protected SymfonyStyle $io;
     protected InputInterface $input;
+    protected OutputInterface $output;
+    protected ProcessHelper $processHelper;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     protected function initialize(InputInterface $input, OutputInterface $output): void
     {
         $this->io = new SymfonyStyle($input, $output);
         $this->input = $input;
+        $this->output = $output;
+        $this->processHelper = $this->getHelper('process');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -61,5 +73,54 @@ abstract class AbstractCommand extends Command implements CommandInterface
         $option = $this->input->getOption($name);
 
         return $option ? $this->getOptionString($name) : null;
+    }
+
+    /**
+     * Execute command in real php sub process.
+     *
+     * @param string[] $args
+     */
+    protected function executeCommand(string $command, array $args): int
+    {
+        $process = new Process(['php', 'bin/console', $command, ...$args]);
+        $process->setTimeout(null);
+        $process->setIdleTimeout(null);
+        $this->io->write(\implode(' ', [$command, ...$args]).': ');
+
+        $this->processHelper->run($this->output, $process, 'Something went wrong!', function () {
+            $this->io->write('*');
+        });
+
+        if ($process->isSuccessful()) {
+            $this->io->write(' SUCCESS');
+        } else {
+            $this->io->write(' ERROR');
+        }
+
+        $this->io->newLine();
+
+        return $process->getExitCode() ?? 0;
+    }
+
+    /**
+     * Run command in same php process.
+     *
+     * @param array<string, mixed> $input
+     */
+    protected function runCommand(string $command, array $input): int
+    {
+        try {
+            if (null === $application = $this->getApplication()) {
+                throw new \RuntimeException('could not find application');
+            }
+
+            $cmd = $application->find($command);
+
+            return $cmd->run(new ArrayInput($input), $this->output);
+        } catch (\Throwable $e) {
+            $this->io->error(\sprintf('Run command failed! (%s)', $e->getMessage()));
+
+            return 0;
+        }
     }
 }
