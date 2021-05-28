9ee2f004a9aca0d2e32c2c96113fdc318626c44f
Mathieu De Keyzer <me@theus.be>
2021-05-28 09:31:36 +0200

    feat: core api upload (#266)
    * feat: hashFile
    
    * refactor: static method to generate init upload url
    
    * refactor: new init upload url
    
    * feat: initUpload in CoreAPI
    
    * feat: postBody in Core Client
    
    * refactor: addChunkUrl method builder
    
    * feat: addChunk in CoreAPI
    
    * refactor: better addChunk signature
    
    * Missing public
    
    Co-authored-by: David mattei <david.mattei@sappico.be>
    
    Co-authored-by: David mattei <david.mattei@sappico.be>

--- src/Common/CoreApi/CoreApiFactory.php
+++ src/Common/CoreApi/CoreApiFactory.php
@@ -6,21 +6,24 @@ namespace EMS\CommonBundle\Common\CoreApi;
 
 use EMS\CommonBundle\Contracts\CoreApi\CoreApiFactoryInterface;
 use EMS\CommonBundle\Contracts\CoreApi\CoreApiInterface;
+use EMS\CommonBundle\Storage\StorageManager;
 use Psr\Log\LoggerInterface;
 
 final class CoreApiFactory implements CoreApiFactoryInterface
 {
     private LoggerInterface $logger;
+    private StorageManager $storageManager;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, StorageManager $storageManager)
     {
         $this->logger = $logger;
+        $this->storageManager = $storageManager;
     }
 
     public function create(string $baseUrl): CoreApiInterface
     {
         $client = new Client($baseUrl, $this->logger);
 
-        return new CoreApi($client);
+        return new CoreApi($client, $this->storageManager);
     }
 }
