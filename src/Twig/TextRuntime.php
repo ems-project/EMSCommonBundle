42ee1341ecf69734625c1f61b41f567b95aebb18
Mathieu De Keyzer <me@theus.be>
2021-09-03 10:45:07 +0200

    refactor: twig runtimes to asset (#296)
    * ref: better type
    
    * chore: depraction
    
    * chore: hide endsWith
    
    * refactoring
    
    * refactoring
    
    * fix: by default it's a png
    
    * ref: overwriteMimeType
    
    * ref: fixFileExtension
    
    * refactoring
    
    * always a hash
    
    * early return
    
    * ref: moved to AssetRuntime
    
    * dep: use the AssetRuntime from twig
    
    * feat: _mime_type config attribute can be used to override the file field's mimetype
    
    * chore: last phpstan issue in runtime services

--- src/Twig/TextRuntime.php
+++ src/Twig/TextRuntime.php
@@ -4,15 +4,14 @@ namespace EMS\CommonBundle\Twig;
 
 use EMS\CommonBundle\Helper\Text\Encoder;
 use EMS\CommonBundle\Json\Decoder;
+use EMS\CommonBundle\Json\JsonMenu;
 use EMS\CommonBundle\Json\JsonMenuNested;
 use Twig\Extension\RuntimeExtensionInterface;
 
 class TextRuntime implements RuntimeExtensionInterface
 {
-    /** @var Encoder */
-    private $encoder;
-    /** @var Decoder */
-    private $decoder;
+    private Encoder $encoder;
+    private Decoder $decoder;
 
     public function __construct(Encoder $encoder, Decoder $decoder)
     {
@@ -20,17 +19,17 @@ class TextRuntime implements RuntimeExtensionInterface
         $this->decoder = $decoder;
     }
 
-    public function htmlEncode(string $text)
+    public function htmlEncode(string $text): string
     {
         return $this->encoder->htmlEncode($text);
     }
 
-    public function htmlEncodePii(string $text)
+    public function htmlEncodePii(string $text): string
     {
         return $this->encoder->htmlEncodePii($text);
     }
 
-    public function jsonMenuDecode(string $json, string $glue = '/')
+    public function jsonMenuDecode(string $json, string $glue = '/'): JsonMenu
     {
         return $this->decoder->jsonMenuDecode($json, $glue);
     }
@@ -40,6 +39,9 @@ class TextRuntime implements RuntimeExtensionInterface
         return $this->decoder->jsonMenuNestedDecode($json);
     }
 
+    /**
+     * @return mixed
+     */
     public function jsonDecode(string $json, bool $assoc = true, int $depth = 512, int $options = 0)
     {
         return \json_decode($json, $assoc, $depth, $options);
