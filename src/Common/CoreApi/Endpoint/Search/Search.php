645a5672a623a818d57e7d127079efcf411c921c
Mathieu De Keyzer <me@theus.be>
2021-11-08 15:03:01 +0100

    feat: count, initScroll, nextScroll, version, healthStatus, refresh, getIndicesFromAlias, getAliasesFromIndex, getDocument sear api

---
+++ src/Common/CoreApi/Endpoint/Search/Search.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CommonBundle\Common\CoreApi\Endpoint\Search;
+
+use EMS\CommonBundle\Common\CoreApi\Client;
+use EMS\CommonBundle\Contracts\CoreApi\Endpoint\Search\SearchInterface;
+use EMS\CommonBundle\Search\Search as SearchObject;
+
+class Search implements SearchInterface
+{
+    private Client $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function search(SearchObject $search): array
+    {
+        return $this->client->post('/api/search/search', ['search' => $search->serialize()])->getData();
+    }
+
+    public function count(SearchObject $search): int
+    {
+        $count = $this->client->post('/api/search/count', ['search' => $search->serialize()])->getData()['count'] ?? null;
+        if (!\is_int($count)) {
+            throw new \RuntimeException('Unexpected: count must be a string');
+        }
+
+        return $count;
+    }
+
+    public function initScroll(SearchObject $search, string $expireTime = '3m'): string
+    {
+        $scrollId = $this->client->post('/api/search/init-scroll', [
+            'search' => $search->serialize(),
+            'expire-time' => $expireTime,
+        ])->getData()['scroll-id'] ?? null;
+        if (!\is_string($scrollId)) {
+            throw new \RuntimeException('Unexpected: scroll-id must be a string');
+        }
+
+        return $scrollId;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function nextScroll(string $scrollId, string $expireTime = '3m'): array
+    {
+        return $this->client->post('/api/search/next-scroll', [
+            'scroll-id' => $scrollId,
+            'expire-time' => $expireTime,
+        ])->getData();
+    }
+
+    public function version(): string
+    {
+        $version = $this->client->get('/api/search/version')->getData()['version'] ?? null;
+        if (!\is_string($version)) {
+            throw new \RuntimeException('Unexpected: search must be a string');
+        }
+
+        return $version;
+    }
+
+    public function healthStatus(): string
+    {
+        $status = $this->client->get('/api/search/health-status')->getData()['status'] ?? null;
+        if (!\is_string($status)) {
+            throw new \RuntimeException('Unexpected: status must be a string');
+        }
+
+        return $status;
+    }
+
+    public function refresh(?string $index = null): bool
+    {
+        $success = $this->client->post('/api/search/refresh', [
+            'index' => $index,
+        ])->getData()['success'] ?? null;
+        if (!\is_bool($success)) {
+            throw new \RuntimeException('Unexpected: search must be a boolean');
+        }
+
+        return $success;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getIndicesFromAlias(string $alias): array
+    {
+        $indices = $this->client->post('/api/search/indices-from-alias', [
+            'alias' => $alias,
+        ])->getData()['indices'] ?? null;
+        if (!\is_array($indices)) {
+            throw new \RuntimeException('Unexpected: search must be an array');
+        }
+
+        return $indices;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAliasesFromIndex(string $index): array
+    {
+        $aliases = $this->client->post('/api/search/aliases-from-index', [
+            'index' => $index,
+        ])->getData()['aliases'] ?? null;
+        if (!\is_array($aliases)) {
+            throw new \RuntimeException('Unexpected: aliases must be an array');
+        }
+
+        return $aliases;
+    }
+
+    /**
+     * @param string[] $sourceIncludes
+     * @param string[] $sourcesExcludes
+     *
+     * @return array<mixed>
+     */
+    public function getDocument(string $index, ?string $contentType, string $id, array $sourceIncludes = [], array $sourcesExcludes = []): array
+    {
+        return $this->client->post('/api/search/document', [
+            'index' => $index,
+            'content-type' => $contentType,
+            'ouuid' => $id,
+            'source-includes' => $sourceIncludes,
+            'sources-excludes' => $sourcesExcludes,
+        ])->getData();
+    }
+}
