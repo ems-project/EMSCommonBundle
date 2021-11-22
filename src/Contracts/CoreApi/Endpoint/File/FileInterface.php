c5c55a78ef9856a8ac3a2859a4f7975ed7e0cfb0
Mathieu De Keyzer <me@theus.be>
2021-11-22 09:18:41 +0100

    feat: file as stream api (#340)
    Co-authored-by: David mattei <david.mattei@sappico.be>

--- src/Contracts/CoreApi/Endpoint/File/FileInterface.php
+++ src/Contracts/CoreApi/Endpoint/File/FileInterface.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace EMS\CommonBundle\Contracts\CoreApi\Endpoint\File;
 
+use Psr\Http\Message\StreamInterface;
+
 interface FileInterface
 {
     public function hashFile(string $filename): string;
+
+    public function hashStream(StreamInterface $stream): string;
 
     public function initUpload(string $hash, int $size, string $filename, string $mimetype): int;
 
@@ -14,5 +18,9 @@ interface FileInterface
 
     public function uploadFile(string $realPath, string $mimeType = null): ?string;
 
+    public function uploadStream(StreamInterface $stream, string $filename, string $mimeType): ?string;
+
     public function headFile(string $realPath): bool;
+
+    public function headHash(string $hash): bool;
 }
