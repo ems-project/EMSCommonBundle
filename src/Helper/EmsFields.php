643df3aea1dc4424f8e46e1965947ee8fe7dbd37
Mathieu De Keyzer <me@theus.be>
2021-12-03 23:01:50 +0100

    fix: ensure that the config hash change if the file change

--- src/Helper/EmsFields.php
+++ src/Helper/EmsFields.php
@@ -44,6 +44,7 @@ final class EmsFields
     public const ASSET_CONFIG_PASSWORD = '_password';
     public const ASSET_CONFIG_AFTER = '_after';
     public const ASSET_CONFIG_BEFORE = '_before';
+    public const ASSET_SEED = '_seed';
 
     public const LOG_ALIAS = 'ems_internal_logger_alias';
     public const LOG_TYPE = 'doc';
