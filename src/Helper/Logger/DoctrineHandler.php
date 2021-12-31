9a2164ad49f03bea52ef57b3d0d2d6a5d597ecf8
Mathieu De Keyzer <me@theus.be>
2021-12-31 12:41:24 +0100

    feat: doctrine monolog handler

---
+++ src/Helper/Logger/DoctrineHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CommonBundle\Helper\Logger;
+
+use Doctrine\ORM\EntityManagerInterface;
+use EMS\CommonBundle\Entity\Log;
+use Monolog\Handler\AbstractProcessingHandler;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class DoctrineHandler extends AbstractProcessingHandler
+{
+    private EntityManagerInterface $entityManager;
+    private TokenStorageInterface $tokenStorage;
+
+    public function __construct(EntityManagerInterface $entityManager, TokenStorageInterface $tokenStorage)
+    {
+        parent::__construct();
+
+        $this->entityManager = $entityManager;
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * @param array{message: string, level: int, level_name: string, context: array, channel: string, formatted: string, datetime: \DateTimeImmutable, extra: array} $record
+     */
+    protected function write(array $record): void
+    {
+        $log = new Log();
+        $log->setMessage($record['message']);
+        $log->setContext($record['context']);
+        $log->setLevel($record['level']);
+        $log->setLevelName($record['level_name']);
+        $log->setChannel($record['channel']);
+        $log->setExtra($record['extra']);
+        $log->setFormatted($record['formatted']);
+
+        $token = $this->tokenStorage->getToken();
+        if ($token instanceof TokenInterface) {
+            $log->setUsername($token->getUsername());
+        }
+
+        $this->entityManager->persist($log);
+        $this->entityManager->flush();
+    }
+}
