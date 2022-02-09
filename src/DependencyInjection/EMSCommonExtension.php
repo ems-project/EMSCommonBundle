29d637beaf031660228e689b03b7686a42574977
Mathieu De Keyzer <me@theus.be>
2022-02-09 09:18:45 +0100

    feat: admin config endpoints (#373)
    * feat: prettier jsons
    
    * feat: index and get ContentType.php
    
    * feat: getConfig
    
    * feat: getConfig
    
    * feat: update content type
    
    * chore: phpcs
    
    * feat: ems:admin:login
    
    * ref: commands.xml
    
    * feat: ems:admin:get
    
    * ref: better exception messsage
    
    * feat: ems:admin:update
    
    * feat: add a most humain readeble getName method to EntityIneterfaces
    
    * rollback: too much impact
    
    * chore: rollback on getName
    
    * ref: no more content-type
    
    * ref: no more content-type
    
    * ref: david's remarks
    
    * ref: david's remarks (again & last?)

--- src/DependencyInjection/EMSCommonExtension.php
+++ src/DependencyInjection/EMSCommonExtension.php
@@ -17,6 +17,7 @@ class EMSCommonExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('contracts.xml');
         $loader->load('services.xml');
+        $loader->load('commands.xml');
         $loader->load('twig.xml');
 
         $configuration = new Configuration();
