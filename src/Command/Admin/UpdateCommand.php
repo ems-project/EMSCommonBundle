af80aa244d7c23109137943b781581dcd469b389
Mathieu De Keyzer <me@theus.be>
2022-02-14 10:21:32 +0100

    feat: update wo json path (#377)
    * fix: update the folder from option if defined
    
    * feat: getFilename from entity's name
    
    * feat: getArgumentStringNull command helper
    
    * fix: --folder option in get commands
    
    * feat: get a default json filename from /opt/src/admin/%config-type%/%entity-name%.json in order to have simplier command: `ems:admin:update content-type route`

--- src/Command/Admin/UpdateCommand.php
+++ src/Command/Admin/UpdateCommand.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace EMS\CommonBundle\Command\Admin;
 
 use EMS\CommonBundle\Common\Admin\AdminHelper;
+use EMS\CommonBundle\Common\Admin\ConfigHelper;
 use EMS\CommonBundle\Common\Command\AbstractCommand;
 use EMS\CommonBundle\Common\Standard\Json;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class UpdateCommand extends AbstractCommand
@@ -16,15 +18,18 @@ class UpdateCommand extends AbstractCommand
     public const CONFIG_TYPE = 'config-type';
     public const ENTITY_NAME = 'entity-name';
     public const JSON_PATH = 'json-path';
+    public const FOLDER = 'folder';
     private string $configType;
     private string $entityName;
-    private string $jsonPath;
+    private ?string $jsonPath;
     private AdminHelper $adminHelper;
+    private string $folder;
 
-    public function __construct(AdminHelper $adminHelper)
+    public function __construct(AdminHelper $adminHelper, string $projectFolder)
     {
         parent::__construct();
         $this->adminHelper = $adminHelper;
+        $this->folder = $projectFolder.DIRECTORY_SEPARATOR.ConfigHelper::DEFAULT_FOLDER;
     }
 
     public function initialize(InputInterface $input, OutputInterface $output): void
@@ -32,7 +37,11 @@ class UpdateCommand extends AbstractCommand
         parent::initialize($input, $output);
         $this->configType = $this->getArgumentString(self::CONFIG_TYPE);
         $this->entityName = $this->getArgumentString(self::ENTITY_NAME);
-        $this->jsonPath = $this->getArgumentString(self::JSON_PATH);
+        $this->jsonPath = $this->getArgumentStringNull(self::JSON_PATH);
+        $folder = $this->getOptionStringNull(self::FOLDER);
+        if (null !== $folder) {
+            $this->folder = $folder;
+        }
     }
 
     protected function configure(): void
@@ -40,7 +49,8 @@ class UpdateCommand extends AbstractCommand
         parent::configure();
         $this->addArgument(self::CONFIG_TYPE, InputArgument::REQUIRED, 'Type of config to update');
         $this->addArgument(self::ENTITY_NAME, InputArgument::REQUIRED, 'Entity\'s name to update');
-        $this->addArgument(self::JSON_PATH, InputArgument::REQUIRED, 'Path to the JSON file');
+        $this->addArgument(self::JSON_PATH, InputArgument::OPTIONAL, 'Path to the JSON file');
+        $this->addOption(self::FOLDER, null, InputOption::VALUE_OPTIONAL, 'Export folder');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -53,6 +63,11 @@ class UpdateCommand extends AbstractCommand
             return self::EXECUTE_ERROR;
         }
         $configApi = $this->adminHelper->getCoreApi()->admin()->getConfig($this->configType);
+        $configHelper = new ConfigHelper($configApi, $this->folder);
+        if (null === $this->jsonPath) {
+            $this->jsonPath = $configHelper->getFilename($this->entityName);
+        }
+
         $fileContent = \file_get_contents($this->jsonPath);
         if (!\is_string($fileContent)) {
             throw new \RuntimeException('Unexpected non string file content');
