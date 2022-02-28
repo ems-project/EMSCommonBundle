73c25ae169f6d8195464971716bb0df6946d2f45
David mattei <d.mattei89@gmail.com>
2022-02-28 14:22:31 +0100

    phpcs

--- src/Common/Standard/Type.php
+++ src/Common/Standard/Type.php
@@ -11,8 +11,8 @@ final class Type
      */
     public static function string($value): string
     {
-        if (!is_string($value)) {
-            throw new \RuntimeException(sprintf("Expect a string got '%s'", gettype($value)));
+        if (!\is_string($value)) {
+            throw new \RuntimeException(\sprintf("Expect a string got '%s'", \gettype($value)));
         }
 
         return $value;
@@ -23,8 +23,8 @@ final class Type
      */
     public static function integer($value): int
     {
-        if (!is_int($value)) {
-            throw new \RuntimeException(sprintf("Expect an integer got '%s'", gettype($value)));
+        if (!\is_int($value)) {
+            throw new \RuntimeException(\sprintf("Expect an integer got '%s'", \gettype($value)));
         }
 
         return $value;
