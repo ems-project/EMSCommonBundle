b00dfe1fb30291070efb92abc40d688c884ab15f
AdrienHq <34314047+AdrienHq@users.noreply.github.com>
2022-03-16 16:43:48 +0100

    fix: phpcs

--- src/Controller/MetricsController.php
+++ src/Controller/MetricsController.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace EMS\CommonBundle\Controller;
 
 use EMS\CommonBundle\Service\Metrics\RendererService;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
-* Class MetricsController.
-*/
+ * Class MetricsController.
+ */
 class MetricsController extends AbstractController
 {
     private RendererService $renderer;
