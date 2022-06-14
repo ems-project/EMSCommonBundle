087dad14eee708c199ab56e3419982d22fea8c85
David mattei <d.mattei89@gmail.com>
2022-06-14 09:01:38 +0200

    refactor(log): localizedLogger extend psr Abstract logger

--- src/Common/Log/LocalizedLogger.php
+++ src/Common/Log/LocalizedLogger.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace EMS\CommonBundle\Common\Log;
 
+use Psr\Log\AbstractLogger;
 use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-class LocalizedLogger implements LoggerInterface
+class LocalizedLogger extends AbstractLogger
 {
     private const PATTERN = '/%(?<parameter>(_|)[[:alnum:]_]*)%/m';
+
     private LoggerInterface $logger;
     private TranslatorInterface $translator;
 
@@ -17,46 +19,6 @@ class LocalizedLogger implements LoggerInterface
     {
         $this->logger = $logger;
         $this->translator = $translator;
-    }
-
-    public function emergency($message, array $context = []): void
-    {
-        $this->logger->emergency($this->translateMessage($message, $context), $context);
-    }
-
-    public function alert($message, array $context = []): void
-    {
-        $this->logger->alert($this->translateMessage($message, $context), $context);
-    }
-
-    public function critical($message, array $context = []): void
-    {
-        $this->logger->critical($this->translateMessage($message, $context), $context);
-    }
-
-    public function error($message, array $context = []): void
-    {
-        $this->logger->error($this->translateMessage($message, $context), $context);
-    }
-
-    public function warning($message, array $context = []): void
-    {
-        $this->logger->warning($this->translateMessage($message, $context), $context);
-    }
-
-    public function notice($message, array $context = []): void
-    {
-        $this->logger->notice($this->translateMessage($message, $context), $context);
-    }
-
-    public function info($message, array $context = []): void
-    {
-        $this->logger->info($this->translateMessage($message, $context), $context);
-    }
-
-    public function debug($message, array $context = []): void
-    {
-        $this->logger->debug($this->translateMessage($message, $context), $context);
     }
 
     public function log($level, $message, array $context = []): void
