e85c07522f665c9efc8543c2879182c314cdde52
David mattei <d.mattei89@gmail.com>
2022-06-14 09:52:32 +0200

    refactor(log): localizedLoggerFactory plus contracts
    ems.logger will be defined in the coreBundle (only used there)

--- src/Common/Log/LocalizedLogger.php
+++ src/Common/Log/LocalizedLogger.php
@@ -4,21 +4,24 @@ declare(strict_types=1);
 
 namespace EMS\CommonBundle\Common\Log;
 
+use EMS\CommonBundle\Contracts\Log\LocalizedLoggerInterface;
 use Psr\Log\AbstractLogger;
 use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-class LocalizedLogger extends AbstractLogger
+class LocalizedLogger extends AbstractLogger implements LocalizedLoggerInterface
 {
     private const PATTERN = '/%(?<parameter>(_|)[[:alnum:]_]*)%/m';
 
     private LoggerInterface $logger;
     private TranslatorInterface $translator;
+    private string $translationDomain;
 
-    public function __construct(LoggerInterface $logger, TranslatorInterface $translator)
+    public function __construct(LoggerInterface $logger, TranslatorInterface $translator, string $translationDomain)
     {
         $this->logger = $logger;
         $this->translator = $translator;
+        $this->translationDomain = $translationDomain;
     }
 
     public function log($level, $message, array $context = []): void
@@ -32,7 +35,7 @@ class LocalizedLogger extends AbstractLogger
     private function translateMessage(string $message, array &$context): string
     {
         $context['translation_message'] = $message;
-        $translation = $this->translator->trans($message, [], 'ems_logger');
+        $translation = $this->translator->trans($message, [], $this->translationDomain);
 
         return \preg_replace_callback(self::PATTERN, function ($match) use ($context) {
             return $context[$match['parameter']] ?? $match['parameter'];
