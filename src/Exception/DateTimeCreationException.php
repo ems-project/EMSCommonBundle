ad7af56c11d959f62012d1bda8802ab51a930152
David mattei <d.mattei89@gmail.com>
2022-11-01 11:38:11 +0100

    chore(phpcs): v3.13

--- src/Exception/DateTimeCreationException.php
+++ src/Exception/DateTimeCreationException.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace EMS\CommonBundle\Exception;
 
-use Throwable;
-
 final class DateTimeCreationException extends \RuntimeException
 {
-    private function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+    private function __construct(string $message = '', int $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
@@ -16,7 +14,7 @@ final class DateTimeCreationException extends \RuntimeException
     /**
      * @param array<mixed> $data
      */
-    public static function fromArray(array $data, string $key, int $code = 0, Throwable $previous = null): DateTimeCreationException
+    public static function fromArray(array $data, string $key, int $code = 0, \Throwable $previous = null): DateTimeCreationException
     {
         $input = $data[$key] ?? '[ERROR: key out of bound]';
         $message = \sprintf('Could not create a DateTime from input value: "%s", with key: "%s"', $input, $key);
